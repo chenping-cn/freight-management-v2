086cacfd7a9525f05bf54103085210135a4b7751
ping_chan@outlook.com <{{email}}>
2025-12-14 22:30:36 +0800

    simplified page_not_found.tsx

--- src/pages/exceptions/PageNotFound.tsx
+++ src/pages/exceptions/PageNotFound.tsx
@@ -10,7 +10,7 @@ function PageNotFound() {
       title="404"
       subTitle="对不起，您访问的页面不存在。"
       extra={
-        <Button type="primary" onClick={() => navigate("/")}>
+        <Button type="primary" onClick={() => navigate("/login")}>
           回到首页
         </Button>
       }
